e55874459cdfb538ea24f7f45f6b419ab45d3c70
Hunter <wilkins.hunter@gmail.com>
2021-09-11 00:19:55 -0500

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,13 @@ function App() {
       <h1>Welcome to Tardy's Tavern</h1>
       <header>
         <h2>Articles</h2>
+        <nav>
+          <a href = '/'>Articles</a>
+          <a href = '/'>Videos</a>
+          <a href = '/'>Gallery</a>
+        </nav>
       </header>
+
       <main>
         <section id = "articles">
           <p>loremipsum</p>
