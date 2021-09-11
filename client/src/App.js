b6de1895635adc0f8dd4e96214d234187627630a
Hunter <wilkins.hunter@gmail.com>
2021-09-11 00:08:44 -0500

    Added basic skeleton and styling, following the wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div id = "content">
+      <h1>Welcome to Tardy's Tavern</h1>
+      <header>
+        <h2>Articles</h2>
+      </header>
+      <main>
+        <section id = "articles">
+          <p>loremipsum</p>
+          <p>lorempigsum</p>
+        </section>
+        <aside>
+          <p>Lorem (aside) ipsum</p>
+        </aside>
+      </main>
     </div>
   );
 }
